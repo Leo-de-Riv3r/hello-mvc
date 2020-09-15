b476bd6cd58e212cb274384771b4ea7143be8e81
Leo-de-Riv3r <leoguti182@gmail.com>
2020-09-15 17:26:36 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const express  = require('express');
+const mongoose = require('mongoose');
+
+
+const port = process.env.PORT        || 3000;
+const db   = process.env.MONGODB_URI || 'mongodb://localhost/hellodb';
+
+const app = express();
+
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+
+const router = require('./routes/index');
+app.use('/', router);
+
+
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useFindAndModify', false);
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => {
+    console.log(`DB conectada @ ${db}`);
+  })
+.catch(err => console.error(`Error de conexion: ${err}`));
+
+
+app.listen(port, () => {
+  console.log(`Servidor alojado en el puerto: ${port}`);
+});
